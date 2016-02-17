6baf5422721907203cad186f2fe6ba509ae2f385
David Gleba <dgleba@gmail.com>
2016-02-16 22:05:24 -0500

    m

--- 21select-local/fltg.py
+++ 21select-local/fltg.py
@@ -8,6 +8,7 @@ http://stackoverflow.com/questions/31081516/how-can-i-turn-a-string-model-field-
 https://gist.github.com/mrjoes/3714266
 http://w3foverflow.com/question/flask-admin-form_override-selectfield-form_args-from-another-view-as-an-in-line-model/
 
+
 '''
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -35,6 +36,9 @@ db = SQLAlchemy(app)
 connection = db.engine.connect()
 # no need to reflect view...
 db.metadata.reflect(bind=db.engine, only=['users','Persons'])
+Base = declarative_base()
+metadata = Base.metadata
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #reflect table...   
 class users(db.Model):
@@ -42,11 +46,21 @@ class users(db.Model):
         autoload=True, autoload_with=db.engine
     )
    
-class persons_mdl(db.Model):
+class Person(db.Model):
     __table__ = db.Table('Persons', db.metadata,
         autoload=True, autoload_with=db.engine
     )
+'''
+class Person(Base):
+    __tablename__ = 'Persons'
 
+    P_Id = Column(Integer, primary_key=True)
+    LastName = Column(String(255), nullable=False)
+    FirstName = Column(String(255))
+    Address = Column(String(255))
+    City = Column(String(255))
+    Group = Column(Text)
+'''    
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Flask views
 @app.route('/')
@@ -59,25 +73,24 @@ class dvview(ModelView):
     column_display_pk = True
 
 class persons_view(dvview):
-    form_overrides = dict(
-        city=SelectField
-    )
-    form_args = dict(
-        city=dict(
-            choices=[
+    create_template = 'dv_editform.html'
+    edit_template = 'dv_editform.html'
+
+    form_overrides = dict(City=SelectField)
+    form_args = dict(City=dict(choices=[
                 ('London1', 'London'),
                 ('Kitchener1', 'Kitchener')
             ]
         )
     )
-    def __init__(self):
-        super(persons_view, self).__init__(persons_mdl, db.session)
+    '''def __init__(self):
+        super(persons_view, self).__init__(persons_mdl, db.session)'''
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Create admin
 admin = admin.Admin(app, name='fltg 21selectlocal', template_mode='bootstrap3')
 
 admin.add_view(dvview(users, db.session))
-admin.add_view(persons_view(persons_mdl, db.session))
+admin.add_view(persons_view(Person, db.session))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if __name__ == '__main__':
     # Start app
