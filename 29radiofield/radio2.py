cf52fbfdd062141a72617ff71184e0df27be433b
dgleba <dgleba@gmail.com>
2016-03-23 14:44:16 -0400

    1. radio field. 2. working on dependent select / cascading select fields

--- 29radiofield/radio2.py
+++ 29radiofield/radio2.py
@@ -26,28 +26,24 @@ class Car(db.Model):
     comment = db.Column(db.Text)
 
 class CarAdmin(ModelView):
-    form_rules = [
-        rules.FieldSet (('desc', 'cartype', rules.HTML('<br><br>'), 'comment'), 'Details')
-    ]
-    form_widget_args = {
-        'cartype': {
-            'style': 'color: black'
-        },
-         'desc': {
-            'style': 'color: black',
-            #'readonly': True
-        },
-         'comment': {
-            'rows': 4,
-            'style': 'color: red',
-        }
-    }
+    # Didn't need this ...
+    #form_create_rules = ('desc', 'cartype', rules.HTML('<br><br>'), 'comment' )  # add some space after cartype...
+        # I didn't get this to work:   rules.Container ( 'CarTypeMacro1', rules.Field('cartype') )
+        
+    # use a radiofield...
     form_overrides = dict(cartype=RadioField)
     form_args = dict(cartype=dict(choices=[
                 ('Compact', 'Compact'),
                 ('Mid-size', 'Mid-size'),
                 ('Pickup-Truck', 'Pickup Truck')
             ]))
+    # Use CSS to change look of the field.  http://stackoverflow.com/questions/31547401/adjust-field-size-in-flask-admin-w-bootstrap3
+    form_widget_args = {
+    'cartype':{'class': 'form-control, width: 1%',
+               #'style': "width: 1%",
+               #'placeholder':'ex. M132 or T456'
+               }
+    }           
      
 # Create admin
 admin = admin.Admin(app, name='Radiofield', template_mode='bootstrap3')
